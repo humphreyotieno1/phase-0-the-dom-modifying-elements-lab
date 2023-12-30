f417ca472f227a655296eff73aa042f810521b10
humphreyotieno1 <humphreyotieno04@gmail.com>
2023-12-30 21:47:35 +0300

    DOM modification

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "YOUR-NAME is the champion";
+document.body.append(newHeader);
+
+const mainElement = document.getElementById('main');
+mainElement.remove('main');
